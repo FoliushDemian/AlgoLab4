c38e849b541ab7ddd7ed4b8f302b35d313eefff8
Demian Foliush <Demian.Foliush.IR.2021@lpnu.ua>
2022-11-01 14:40:49 +0200

    add lab4

---
+++ src/main/java/ua/lviv/iot/manager/Solver.java
@@ -0,0 +1,57 @@
+package ua.lviv.iot.manager;
+
+import java.util.LinkedList;
+
+public class Solver {
+
+    private static LinkedList<String> track = new LinkedList<>();// для виводу рядка
+    // то не обовязково
+    public static void solve(LinkedList<String> dictionary) {
+        int result = 0;
+        Integer cash;
+        for (String word : dictionary) {
+            cash = check(dictionary, word, 0, new LinkedList<>());
+            if (result < cash) {
+                result = cash;
+            }
+        }
+
+        int counter = 0;
+        for (String word : track) {
+            counter++;
+            System.out.println(counter + ": " + word);
+        }
+        ResultWriter.writeToFile(result);
+    }
+
+
+    private static Integer check(LinkedList<String> dictionary, String word, Integer depth, LinkedList<String> localTrack) {
+        int result = depth;
+        if (dictionary.contains(word)) {
+            localTrack.add(word);
+            if (localTrack.size() > track.size()) {
+                track = localTrack;
+            }
+            depth++;
+            result = depth;
+            Integer cash;
+            for (int i = 0; i < word.length(); i++) {
+                cash = check(dictionary, newWord(word, i), depth, new LinkedList<>(localTrack));
+                if (result < cash) {
+                    result = cash;
+                }
+            }
+        }
+        return result;
+    }
+
+    public static String newWord(String word, Integer index) {
+        StringBuilder newWord = new StringBuilder();
+        for (int i = 0; i < word.length(); i++) {
+            if (i != index) {
+                newWord.append(word.charAt(i));
+            }
+        }
+        return newWord.toString();
+    }
+}
